55a6c0765052e59f4e3c4796c00429e1ad3f7e7c
joshua.software.dev <joshua.software.dev@gmail.com>
2020-08-02 14:31:41 -0700

    Remove API cache json serialization
    Pickling works just fine.

--- ff14angler/__main__.py
+++ ff14angler/__main__.py
@@ -1,11 +1,9 @@
 #! /usr/bin/env python3
 
 import asyncio
-import json
 import pickle
 
 from ff14angler.network.chromeWrapper import ChromeWrapper
-from ff14angler.dunderSerializer import DunderSerializer
 from ff14angler.fetch.fetch import Fetch
 from ff14angler.xivapi import XivApi
 
@@ -28,7 +26,3 @@ def main():
         print('Writing binary API cache to disk...')
         with open('data/xivapi_cache.pickle', 'wb+') as fh:
             pickle.dump(XivApi.cached_responses, fh)
-
-        print('Writing text API cache to disk...')
-        with open('data/xivapi_cache.json', 'w+') as fh:
-            json.dump(XivApi.cached_responses, fh, cls=DunderSerializer, indent=4)
