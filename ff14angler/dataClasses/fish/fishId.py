bca3188531780e26c7341700a8bf0255ccca31d7
joshua.software.dev <joshua.software.dev@gmail.com>
2020-08-02 14:41:32 -0700

    Update fish_id to have prepended unique name to keys

--- ff14angler/dataClasses/fish/fishId.py
+++ ff14angler/dataClasses/fish/fishId.py
@@ -7,8 +7,8 @@ from ff14angler.aiohttpWrapped import AiohttpWrapped
 
 @dataclass
 class FishId:
-    angler_fish_id: int
-    xivapi_item_id: int
+    fish_angler_fish_id: int
+    fish_xivapi_item_id: int
 
     def __json__(self):
         return self.__dict__
@@ -16,4 +16,4 @@ class FishId:
     @classmethod
     async def get_fish_id_from_angler_fish(cls, fish_angler_id: int, fish_angler_name: str):
         search_response = await AiohttpWrapped.xivapi_item_search(fish_angler_name)
-        return cls(angler_fish_id=fish_angler_id, xivapi_item_id=search_response['ID'])
+        return cls(fish_angler_fish_id=fish_angler_id, fish_xivapi_item_id=search_response['ID'])
