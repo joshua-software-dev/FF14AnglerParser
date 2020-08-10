2e6702e8cae1a23ac18129c736ca01472515ff16
joshua.software.dev <joshua.software.dev@gmail.com>
2020-08-10 03:01:47 -0700

    Add export directory to constants
    Makes it easier to change.

--- ff14angler/fetch/fetch.py
+++ ff14angler/fetch/fetch.py
@@ -5,6 +5,7 @@ import os
 
 from selenium.webdriver.chrome.webdriver import WebDriver  # type: ignore
 
+from ff14angler.constants.values import EXPORT_DIRECTORY
 from ff14angler.export.dunderSerializer import DunderSerializer
 from ff14angler.export.tableExport import TableExport
 from ff14angler.fetch.baitPage import BaitPage
@@ -24,28 +25,31 @@ class Fetch:
         await SpotPage.collect_spot_data(driver)
 
         print('Writing JSON dump form scraping results...')
-        os.makedirs('data', exist_ok=True)
-        with open('data/full_dump.json', 'w+') as fh:
+        os.makedirs(EXPORT_DIRECTORY, exist_ok=True)
+        with open(os.path.join(EXPORT_DIRECTORY, 'full_dump.json'), 'w+') as fh:
             json.dump(scraping_data, fh, cls=DunderSerializer, indent=4, sort_keys=True)
 
         print('Writing table form scraping results...')
-        os.makedirs('data/table_data')
+        os.makedirs(os.path.join(EXPORT_DIRECTORY, 'table_data'))
         await TableExport.output_data_for_database(scraping_data)
 
         print('Writing bait one entry per file scraping results...')
-        os.makedirs('data/bait_data', exist_ok=True)
+        bait_dir = os.path.join(EXPORT_DIRECTORY, 'bait_data')
+        os.makedirs(bait_dir, exist_ok=True)
         for angler_bait_id, bait_data in scraping_data.bait.items():
-            with open(f'data/bait_data/{angler_bait_id}.json', 'w+') as fh:
+            with open(os.path.join(bait_dir, f'{angler_bait_id}.json'), 'w+') as fh:
                 json.dump(bait_data, fh, cls=DunderSerializer, indent=4, sort_keys=True)
 
         print('Writing fish one entry per file scraping results...')
-        os.makedirs('data/fish_data', exist_ok=True)
+        fish_dir = os.path.join(EXPORT_DIRECTORY, 'fish_data')
+        os.makedirs(fish_dir, exist_ok=True)
         for angler_fish_id, fish_data in scraping_data.fish.items():
-            with open(f'data/fish_data/{angler_fish_id}.json', 'w+') as fh:
+            with open(os.path.join(fish_dir, f'{angler_fish_id}.json'), 'w+') as fh:
                 json.dump(fish_data, fh, cls=DunderSerializer, indent=4, sort_keys=True)
 
         print('Writing spot one entry per file scraping results...')
-        os.makedirs('data/spot_data', exist_ok=True)
+        spot_dir = os.path.join(EXPORT_DIRECTORY, 'spot_data')
+        os.makedirs(spot_dir, exist_ok=True)
         for angler_spot_id, spot_data in scraping_data.spot.items():
-            with open(f'data/spot_data/{angler_spot_id}.json', 'w+') as fh:
+            with open(os.path.join(spot_dir, f'{angler_spot_id}.json'), 'w+') as fh:
                 json.dump(spot_data, fh, cls=DunderSerializer, indent=4, sort_keys=True)
