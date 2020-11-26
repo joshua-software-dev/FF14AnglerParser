5620dac3e3cf088b5fba1f09e80d3a43f5ffe067
joshua.software.dev <joshua.software.dev@gmail.com>
2020-11-26 01:27:24 -0800

    Fix fish bait preference scraping not correctly scraping mooch fish

--- ff14angler/dataClasses/bait/baitProvider.py
+++ ff14angler/dataClasses/bait/baitProvider.py
@@ -1,11 +1,11 @@
 #! /usr/bin/env python3
 
-from typing import Dict, List
+from typing import Dict, List, Optional
 
 from bs4 import BeautifulSoup  # type: ignore
 from bs4.element import Tag  # type: ignore
 
-from ff14angler.constants.regex import non_number_replacement_regex
+from ff14angler.constants.regex import mooch_name_regex, non_number_replacement_regex
 from ff14angler.dataClasses.bait.bait import Bait
 from ff14angler.dataClasses.bait.baitPercentage import BaitPercentage
 
@@ -35,11 +35,20 @@ class BaitProvider:
     async def get_bait_percentage_list_from_fish_soup(cls, soup: BeautifulSoup) -> List[BaitPercentage]:
         temp_bait_list: List[BaitPercentage] = []
 
-        for tr in soup.find_all('tr', {'class': 'bait'}):  # type: Tag
-            td1, td2, _, td4 = tr.find_all('td')  # type: Tag, Tag, _, Tag
+        bait_section: Optional[Tag] = soup.find('form', {'name': 'bait_delete'})
+        if not bait_section:
+            return temp_bait_list
+
+        for child in bait_section.find_all('td', {'class': 'width_max'}):  # type: Tag
+            parent: Tag = child.parent
+            td1, td2 = parent.find_all('td')[:2]  # type: Tag, Tag
             angler_bait_id: int = await cls._parse_angler_bait_id(td2)
-            angler_bait_name: str = td2.text.strip()
+            angler_bait_name: str = mooch_name_regex.sub('', td2.text).strip()
             bait = await cls.get_bait_from_angler_bait(angler_bait_id, angler_bait_name)
+
+            if bait.bait_item_name_en is None:
+                await bait.update_bait_with_assume_is_mooch_fish()
+
             temp_bait_list.append(
                 BaitPercentage(
                     bait_id=bait.bait_id,
