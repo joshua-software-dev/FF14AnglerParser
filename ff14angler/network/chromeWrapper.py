e39d897f9c5c1ba0771852fd1d4d7508a1119f52
joshua.software.dev <joshua.software.dev@gmail.com>
2020-11-26 08:22:53 -0800

    Clean up chromeWrapper and make it work with headless-chrome
    I wish I knew it was as easy as `yum install chromium-headless` to get a headless chrome without a bunch of desktop dependencies on CentOS before I tried making a wrapper around CEF.

--- ff14angler/network/chromeWrapper.py
+++ ff14angler/network/chromeWrapper.py
@@ -2,57 +2,66 @@
 
 import os
 
+from typing import Optional
+
 from selenium import webdriver  # type: ignore
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.webdriver import WebDriver
 
 
 class ChromeWrapper:
-    """Selenium chrome driver wrapper so I stop making one off selenium setups."""
+    """Selenium chromedriver wrapper so I stop making one off selenium setups."""
 
-    def __init__(self, download_directory=False, headless=False, manual=False, maximized=True):
+    def __init__(
+        self,
+        download_directory: Optional[str] = None,
+        headless: bool = True,
+        manual_binary_path: Optional[str] = None,
+        start_maximized: bool = True
+    ):
         """Init for ChromeWrapper."""
-        self.options = self.setup_config_options(download_directory, headless, maximized)
-        self.get_chrome_install_location(manual)
-
-        self.driver = webdriver.Chrome(chrome_options=self.options)
-        self.driver.find_elements_by_css_selector('li.selected div.layer-toggle')
-        self.enable_download(download_directory)
+        self.driver: Optional[WebDriver] = None
+        self.download_directory: Optional[str] = download_directory
+        self.options = self.setup_config_options(
+            download_directory,
+            headless,
+            manual_binary_path,
+            start_maximized
+        )
 
     def __enter__(self):
         """Make driver available in a with context.."""
+        self.driver = webdriver.Chrome(chrome_options=self.options)
+
+        if self.download_directory is not None:
+            self.enable_download(self.driver, self.download_directory)
+
         return self.driver
 
     def __exit__(self, exception_type, exception_val, trace):
         """Properly close driver."""
         self.driver.quit()
 
-    @staticmethod
-    def setup_config_options(download_directory, headless, maximized):
+    @classmethod
+    def setup_config_options(
+        cls,
+        download_directory: Optional[str],
+        headless: bool,
+        manual_binary_path: Optional[str],
+        start_maximized: bool
+    ) -> Options:
         """Setup default chrome config options."""
         options = webdriver.ChromeOptions()
-
-        # noinspection SpellCheckingInspection
-        config = {
-            'profile.default_content_settings.popups': 0,
-            'download.prompt_for_download': 'false',
-            'profile.default_content_setting_values.automatic_downloads': True,
-            'safebrowsing.enabled': False
-        }
-
-        if download_directory:
-
-            config['download.default_directory'] = download_directory
-
-        if headless:
-
-            options.add_argument('--headless')
-
-        if maximized:
-
-            options.add_argument('--start-maximized')
-
+        options.add_argument('--allow-running-insecure-content')
         options.add_argument('--disable-gpu')
         options.add_argument('--disable-web-security')
-        options.add_argument('--allow-running-insecure-content')
+
+        if headless:
+            options.add_argument('--headless')
+
+        if start_maximized:
+            options.add_argument('--start-maximized')
+
         # noinspection SpellCheckingInspection
         options.add_argument(
             '--user-agent={}'.format(
@@ -60,44 +69,58 @@ class ChromeWrapper:
                 '(KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36'
             )
         )
+
+        # noinspection SpellCheckingInspection
+        config = {
+            'download.prompt_for_download': False,
+            'profile.default_content_settings.popups': 0,
+            'profile.default_content_setting_values.automatic_downloads': True,
+            'safebrowsing.enabled': False
+        }
+
+        if download_directory:
+            config['download.default_directory'] = download_directory
+
         # noinspection SpellCheckingInspection
         options.add_experimental_option('prefs', config)
+        return cls.get_binary_location(options, manual_binary_path)
 
-        return options
+    @staticmethod
+    def get_binary_location(options: Options, manual_binary_path: Optional[str]) -> Options:
+        """Get the install location for the cef executable."""
+        if manual_binary_path is not None:
 
-    def get_chrome_install_location(self, manual):
-        """Get the install location for the chrome executable."""
-        if manual:
-
-            if os.path.isfile(manual):
-
-                self.options.binary_location = manual
-
+            if os.path.isfile(manual_binary_path):
+                options.binary_location = manual_binary_path
             else:
-
                 raise FileNotFoundError(
-                    'The manual chrome executable location was not found: {}'.format(manual)
+                    'The manual chrome/chromium executable location was not found: {}'.format(manual_binary_path)
                 )
 
-        default_binary_paths = ['/usr/bin/google-chrome', '/usr/bin/google-chrome-stable', '/usr/bin/chromium']
+        default_binary_paths = [
+            '/usr/bin/google-chrome',
+            '/usr/bin/google-chrome-stable',
+            '/usr/bin/chromium',
+            '/usr/lib64/chromium-browser/headless_shell'
+        ]
 
         for binary in default_binary_paths:
             if os.path.isfile(binary):
-                self.options.binary_location = binary
-                return
+                options.binary_location = binary
+                return options
 
-        raise FileNotFoundError('Chrome install file not found.')
+        raise FileNotFoundError('Chrome/Chromium binary file not found.')
 
-    def enable_download(self, download_directory):
+    @staticmethod
+    def enable_download(driver: WebDriver, download_directory: Optional[str]):
         """Add missing support for chrome "send_command" to selenium web driver."""
         if download_directory:
-
             # noinspection PyProtectedMember
-            self.driver.command_executor._commands['send_command'] = (
+            driver.command_executor._commands['send_command'] = (
                 'POST', '/session/$sessionId/chromium/send_command'
             )
 
-            self.driver.execute(
+            driver.execute(
                 'send_command',
                 {
                     'cmd': 'Page.setDownloadBehavior',
